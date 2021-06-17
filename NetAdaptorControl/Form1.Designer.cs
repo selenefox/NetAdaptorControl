9ef4c8258bafcd110ab328e8230e69abb74cdfb5
SevilinMa <selene_fox@hotmail.com>
2021-06-17 09:40:17 +0800

    Project Import to GIT

---
+++ NetAdaptorControl/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace NetAdaptorControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.enableButton = new System.Windows.Forms.Button();
+            this.disableButton = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(763, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "刷新设备";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(744, 356);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "链接名称";
+            this.columnHeader1.Width = 122;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "设备名";
+            this.columnHeader2.Width = 321;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "厂家";
+            this.columnHeader3.Width = 110;
+            // 
+            // enableButton
+            // 
+            this.enableButton.Enabled = false;
+            this.enableButton.Location = new System.Drawing.Point(763, 54);
+            this.enableButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.enableButton.Name = "enableButton";
+            this.enableButton.Size = new System.Drawing.Size(111, 39);
+            this.enableButton.TabIndex = 2;
+            this.enableButton.Text = "激活设备";
+            this.enableButton.UseVisualStyleBackColor = true;
+            this.enableButton.Click += new System.EventHandler(this.enableButton_Click);
+            // 
+            // disableButton
+            // 
+            this.disableButton.Enabled = false;
+            this.disableButton.Location = new System.Drawing.Point(763, 99);
+            this.disableButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.disableButton.Name = "disableButton";
+            this.disableButton.Size = new System.Drawing.Size(111, 39);
+            this.disableButton.TabIndex = 3;
+            this.disableButton.Text = "禁用设备";
+            this.disableButton.UseVisualStyleBackColor = true;
+            this.disableButton.Click += new System.EventHandler(this.disableButton_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "网络链接管理器 - v1.0.0";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 384);
+            this.Controls.Add(this.disableButton);
+            this.Controls.Add(this.enableButton);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "网络链接管理器 - v1.0.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button enableButton;
+        private System.Windows.Forms.Button disableButton;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ NetAdaptorControl/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Management.PropertyDataCollection;
+
+namespace NetAdaptorControl
+{
+    public partial class Form1 : Form
+    {
+        private NetAdapteorInfo currSelectedNetAdapteorInfo = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefreshAllDevices();
+        }
+
+        private void enableButton_Click(object sender, EventArgs e)
+        {
+            if(currSelectedNetAdapteorInfo != null)
+            {
+                EnableAdapterByDeviceId(currSelectedNetAdapteorInfo.DeviceId);
+                Trace.TraceInformation("EnableAdapterByDeviceId - DeviceId:", currSelectedNetAdapteorInfo.DeviceId);
+            }
+        }
+
+        private void disableButton_Click(object sender, EventArgs e)
+        {
+            if (currSelectedNetAdapteorInfo != null)
+            {
+                DisableAdapterByDeviceId(currSelectedNetAdapteorInfo.DeviceId);
+                Trace.TraceInformation("DisableAdapterByDeviceId - DeviceId:", currSelectedNetAdapteorInfo.DeviceId);
+            }
+        }
+
+        private void NetAdaptor()
+        {
+            //NetAdaptorControl.Ne
+            //获取说有网卡信息
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in nics)
+            {
+                Console.WriteLine("All ::" + adapter.Name);
+                //判断是否为以太网卡
+                //Wireless80211         无线网卡    Ppp     宽带连接
+                //Ethernet              以太网卡   
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                {
+                    Trace.WriteLine(adapter.ToString() + "::" + adapter.Name + "-" + adapter.OperationalStatus);
+                }
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    Trace.WriteLine(adapter.ToString() + "::" + adapter.Name + "-" + adapter.OperationalStatus);
+                }
+            }
+        }
+
+        public void RefreshAllDevices()
+        {
+            
+            listView1.BeginUpdate();
+            listView1.Items.Clear();
+            System.Management.ManagementObjectSearcher moc = new System.Management.ManagementObjectSearcher("Select * from Win32_NetworkAdapter where NetEnabled!=null ");
+            foreach (System.Management.ManagementObject mo in moc.Get())
+            {
+                /*
+                if (mo.Properties.Count > 0)
+                {
+                    // debug tools print mo data all Properties 
+                    PropertyDataEnumerator enumerator = mo.Properties.GetEnumerator();
+                    while (enumerator.MoveNext())
+                    {
+                        Type type = enumerator.GetType();
+                        PropertyData obj = enumerator.Current;
+
+                        Trace.TraceInformation("Name:{0} Object:{1}", obj.Name, obj.Value);
+                    }
+
+                }
+                */
+                string Manufacturer = mo["Manufacturer"].ToString();
+                string DeviceId = mo["DeviceID"].ToString();
+                string NetConnectionID = mo["NetConnectionID"].ToString();
+                string ProductName = mo["ProductName"].ToString();
+
+                Trace.TraceInformation("Manufacturer={0}, ProductName={1}, NetConnectionID={2}, DeviceId={3}", Manufacturer, ProductName, NetConnectionID, DeviceId);
+
+                listView1.Items.Add(new NetAdaptorListViewItem().BindData(new NetAdapteorInfo(DeviceId,NetConnectionID, Manufacturer, ProductName)));
+                
+            }
+            listView1.EndUpdate();
+
+        }
+
+        /// <summary>
+        /// 启用所有适配器
+        /// </summary>
+        /// <returns></returns>
+        public void EnableAdapterByDeviceId(string DeviceId)
+        {
+            System.Management.ManagementObjectSearcher moc = new System.Management.ManagementObjectSearcher("Select * from Win32_NetworkAdapter where NetEnabled!=null ");
+            foreach (System.Management.ManagementObject mo in moc.Get())
+            {
+                string did = mo["DeviceID"].ToString();
+                if (did.Equals(DeviceId))
+                {
+                    mo.InvokeMethod("Enable", null);
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 禁用所有适配器
+        /// </summary>
+        public void DisableAdapterByDeviceId(string DeviceId)
+        {
+            System.Management.ManagementObjectSearcher moc = new System.Management.ManagementObjectSearcher("Select * from Win32_NetworkAdapter where NetEnabled!=null ");
+            foreach (System.Management.ManagementObject mo in moc.Get())
+            {
+                string did = mo["DeviceID"].ToString();
+                if (did.Equals(DeviceId))
+                {
+                    mo.InvokeMethod("Disable", null);
+                    break;
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int count = listView1.SelectedItems.Count;
+            if(count > 0)
+            {
+                ButtonSetupEnable(true);
+                currSelectedNetAdapteorInfo = ((NetAdaptorListViewItem)listView1.SelectedItems[0]).ItemInfo;
+                Trace.TraceInformation("SelectCount:{0}, SelectIndex:{1}", listView1.SelectedItems.Count, listView1.SelectedItems[0].Index);
+            }
+            else
+            {
+                ButtonSetupEnable(false);
+                currSelectedNetAdapteorInfo = null;
+                Trace.TraceInformation("SelectCount:{0}", listView1.SelectedItems.Count);
+            }
+        }
+
+        private void ButtonSetupEnable(bool enable)
+        {
+            enableButton.Enabled = enable;
+            disableButton.Enabled = enable;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r = MessageBox.Show("你是否要退出程序吗？“是”退出并关闭程序，“否”将缩小到状态栏。", "提示", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (r == DialogResult.Yes)
+            {
+                notifyIcon1.Visible = false;
+                e.Cancel = false;
+            }
+            else if(r == DialogResult.No)
+            {
+                this.Hide();
+                e.Cancel = true;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshAllDevices();
+            notifyIcon1.Visible = true;
+        }
+    }
+
+    public class NetAdapteorInfo
+    {
+        public string DeviceId;
+        public string NetConnectionID;
+        public string Manufacturer;
+        public string ProductName;
+
+        public NetAdapteorInfo(string deviceId, string netConnectionID, string manufacturer, string productName)
+        {
+            DeviceId = deviceId;
+            NetConnectionID = netConnectionID;
+            Manufacturer = manufacturer;
+            ProductName = productName;
+        }
+    }
+
+    public class NetAdaptorListViewItem : ListViewItem
+    {
+        public NetAdapteorInfo ItemInfo { get; private set; }
+
+        public NetAdaptorListViewItem BindData(NetAdapteorInfo iteminfo)
+        {
+            ItemInfo = iteminfo;
+            this.Text = ItemInfo.NetConnectionID;
+            this.SubItems.Add(ItemInfo.ProductName);
+            this.SubItems.Add(ItemInfo.Manufacturer);
+            return this;
+        }
+
+        public void Refresh(NetAdapteorInfo iteminfo = null)
+        {
+            if (iteminfo != null)
+            {
+                ItemInfo = iteminfo;
+            }
+            this.Text = ItemInfo.NetConnectionID;
+            this.SubItems[1].Text = ItemInfo.ProductName;
+            this.SubItems[2].Text = ItemInfo.Manufacturer;
+        }
+    }
+}
